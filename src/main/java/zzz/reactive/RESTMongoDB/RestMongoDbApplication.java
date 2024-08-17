4ea1d19ab65a15667bc08edce06d252c865835ef
mateo.nihil <mateo.nihil@gmail.com>
2024-08-17 04:58:41 -0500

    1. Initial

---
+++ src/main/java/zzz/reactive/RESTMongoDB/RestMongoDbApplication.java
@@ -0,0 +1,13 @@
+package zzz.reactive.RESTMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zzz/reactive/RESTMongoDB/RestMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package zzz.reactive.RESTMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
